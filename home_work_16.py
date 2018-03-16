609193cfaa0b11bbde2407c7b5e65a125495703c
Oleg <exupery000@ukr.net>
2018-03-17 00:07:18 +0200

    hw

---
+++ home_work_16.py
@@ -0,0 +1,34 @@
+# hw16
+# Два поезда движутся на скорости V1 и V2 навстречу друг другу.
+# Между ними 10 км. пути. Через 4 км пути первый поезд может свернуть на запасной путь.
+# При заданных скоростях узнать столкнутся ли поезда.
+# def have_trains_crashed(v1, v2): # returns boolean value
+#           pass
+
+
+def have_trains_crashed(v1, v2):
+    if v2 >= 1.5 * v1:
+        return True
+    else:
+        return False
+
+
+print('Задание 16'
+      '\nДва поезда движутся на скорости V1 и V2 навстречу друг другу.'
+      '\nМежду ними 10 км. пути. Через 4 км пути первый поезд может свернуть на запасной путь.'
+      '\nПри заданных скоростях узнать столкнутся ли поезда.'
+      '\n'
+      '\ndef have_trains_crashed(v1, v2): # returns boolean value'
+      '\n       pass'
+      '\n')
+
+
+velocity1 = int(input('Enter Velocity1: '))
+velocity2 = int(input('Enter Velocity2: '))
+if have_trains_crashed(velocity1, velocity2):
+    print('Столкновение неизбежно!!!')
+    print('Столкновение неизбежно!!!')
+    print('Столкновение неизбежно!!!')
+
+else:
+    print('Путь свободен. Двигайтесь с указанной скоростью')
