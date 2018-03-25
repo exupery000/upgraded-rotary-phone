11ffca2b9f3955166926898f875a0f29105cfddc
Oleg <exupery000@ukr.net>
2018-03-25 14:33:45 +0300

    hw

--- home_work_23.py
+++ home_work_23.py
@@ -10,39 +10,50 @@
 #     pass
 
 
-# print('Задание 23'
-#       '\nСогласно древней индийской легенде создатель шахмат за своё изобретение попросил у раджи незначительную,'
-#       '\nна первый взгляд, награду: столько пшеничных зёрен, сколько окажется на шахматной доске,'
-#       '\nесли на первую клетку положить одно зерно, на вторую — два зерна, на третью — четыре зерна и т. д.'
-#       '\nОказалось, что такого количества зерна нет на всей планете (оно равно 2**64 − 1 зерен.'
-#       '\nПосчитайте, начиная с какой клетки по счету, общее количество зерен,'
-#       '\nкоторое должен был бы отдать раджа изобретателю было больше 1 000 000 зерен'
-#       '\nи сколько конкретно зерен он должен был бы отдать.'
-#       '\ndef chess_reward(): # returns 2 ints (cell number and total number of corns)'
-#       '\n       pass'
-#       '\n')
-#
+print('Задание 23'
+      '\nСогласно древней индийской легенде создатель шахмат за своё изобретение попросил у раджи незначительную,'
+      '\nна первый взгляд, награду: столько пшеничных зёрен, сколько окажется на шахматной доске,'
+      '\nесли на первую клетку положить одно зерно, на вторую — два зерна, на третью — четыре зерна и т. д.'
+      '\nОказалось, что такого количества зерна нет на всей планете (оно равно 2**64 − 1 зерен.'
+      '\nПосчитайте, начиная с какой клетки по счету, общее количество зерен,'
+      '\nкоторое должен был бы отдать раджа изобретателю было больше 1 000 000 зерен'
+      '\nи сколько конкретно зерен он должен был бы отдать.'
+      '\ndef chess_reward(): # returns 2 ints (cell number and total number of corns)'
+      '\n       pass'
+      '\n')
 
 
-# def chess_reward():
 
-def chess_table(n, m):
+def chess_reward():
+    cell_number = 0
+    print('\n-----------')
+    print('Chess Table:')
+    print('-----------')
+    for i in range(1, 8 + 1):
+        for j in range(1, 8 + 1):
+            cell_number += 1
+            print('%4d' % cell_number, end=' ')
+        print()
+
+    print()
     cell_number = 0
     cur_number_of_corns = 0
-    total_number_of_corns = 0
-    print('\n-----------')
-    print('Pyph Table:')
-    print('-----------')
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            cur_number_of_corns = 2 ** cell_number
-            cell_number += 1
-            total_number_of_corns += cur_number_of_corns
-            print(cell_number, cur_number_of_corns, total_number_of_corns)
-            if total_number_of_corns >= 1000000:
-                print('c этого момента')
-                print()
-    return cell_number, total_number_of_corns
-    print()
-print(chess_table(8, 8))
+    total_number_of_corns =0
+    for i in range(1, 8 + 1):
+            for j in range(1, 8 + 1):
+                cur_number_of_corns = 2 ** cell_number
+                cell_number += 1
+                total_number_of_corns += cur_number_of_corns
+                print('Номер клетки: %d, '
+                      'колличество зерен на клетке: %d, '
+                      'общее колличество зерен в этот момент: %d' %
+                      (cell_number, cur_number_of_corns, total_number_of_corns))
+                if total_number_of_corns >= 1000000:
+                    print('Вот он! Момент "М"')
+                    print()
+                    return cell_number, total_number_of_corns
 
+
+res1, res2 = chess_reward()
+print('Начиная с клетки № %d (включительно) общее колличество зерен будет: %d '
+      '\nи продолжит увеличиваться каждый раз в двое' % (res1, res2))
