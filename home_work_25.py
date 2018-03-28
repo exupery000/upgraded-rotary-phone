94f82fedcad0e8380d9e913757c3213865ea17cc
Oleg <exupery000@ukr.net>
2018-03-28 21:25:06 +0300

    hw

---
+++ home_work_25.py
@@ -0,0 +1,45 @@
+# hw 25
+# Создайте список на 50 элементов из всех нечётных чисел от 1 до 99 и передайте его в функцию,
+# которая переставляет его элементы в случайном порядке (например, 99 11 43 19 … 7 91 3 1).
+# Примечание: использовать метод random.shuffle не допускается.
+# def shuffle_list(list_to_shuffle): # no return (shuffles list in place)
+#     pass
+
+
+print('Задание 25'
+      '\nСоздайте список на 50 элементов из всех нечётных чисел от 1 до 99 и передайте его в функцию,'
+      '\nкоторая переставляет его элементы в случайном порядке (например, 99 11 43 19 … 7 91 3 1).'
+      '\nПримечание: использовать метод random.shuffle не допускается.'
+      '\n'
+      '\ndef shuffle_list(list_to_shuffle): # no return (shuffles list in place)'
+      '\n       pass'
+      '\n')
+
+import random
+
+lst = [i for i in range(1, 100) if i % 2 != 0]
+print('Создадим список, длиной в %d элементов' % (len(lst)))
+print(lst)
+print('------------------------')
+
+
+def shuffle_lst(lst):
+    base_lst = lst
+    new_lst = []
+    while len(new_lst) != len(base_lst):
+        for element in lst:
+            element = random.choice(lst)
+            if element not in new_lst:
+                new_lst.append(element)
+    print('Сравним длины базового списка', len(base_lst))
+    print('И Нового перемешанного списка', len(new_lst))
+    print('------------------------')
+    return new_lst
+
+
+print('Сравним сами списки'
+      '\nБазовый'
+      '\n', shuffle_lst(lst))
+print('------------------------')
+print('Новый'
+      '\n', lst)
